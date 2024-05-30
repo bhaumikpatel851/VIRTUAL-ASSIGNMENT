f61bea5a1d5b6b4fb06198640ee947381c42f396
bhaumikpatel851 <134671922+bhaumikpatel851@users.noreply.github.com>
2024-05-30 12:20:59 -0400

    VirtualPet-Part2
    Here, In this there is codeing of feed, play, rest, checkstatus of the praticuar pet so I have pushed this code to github for it.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,5 +18,34 @@ namespace VirtualPet
             Happiness = 5;
             Health = 10;
         }
+
+        public void Feed()
+        {
+            Hunger = Math.Max(0, Hunger - 2);
+            Happiness = Math.Min(10, Happiness + 1);
+            Console.WriteLine("You feed {0}. Hunger decreases.", Name);
+        }
+        public void Play()
+        {
+            Happiness = Math.Min(10, Happiness + 2);
+            Hunger = Math.Min(10, Hunger + 1);
+            Console.WriteLine("You play with {0}. Happiness increases.", Name);
+        }
+
+        public void Rest()
+        {
+            Health = Math.Min(10, Health + 2);
+            Happiness = Math.Max(0, Happiness - 1);
+            Console.WriteLine("You let {0} rest. Health improves.", Name);
+        }
+
+        public void CheckStatus()
+        {
+            Console.WriteLine("status:", Name);
+            Console.WriteLine("• Hunger: {0}", Hunger);
+            Console.WriteLine("• Happiness: {0}", Happiness);
+            Console.WriteLine("• Health: {0}", Health);
+        }
     }
+
 }
