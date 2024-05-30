6633a3083996338326d697c91d2a86e0435c9ee7
bhaumikpatel851 <134671922+bhaumikpatel851@users.noreply.github.com>
2024-05-30 11:10:34 -0400

    VitualPet-Part1
    Firstly, I have done some basic concepts of the pet which I have done that  pushing to Github.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VirtualPet
+{
+    class Pet
+    {
+        public string Name { get; }
+        public string Type { get; }
+        public int Hunger { get; private set; }
+        public int Happiness { get; private set; }
+        public int Health { get; private set; }
+
+        public Pet(string name, string type)
+        {
+            Name = name;
+            Type = type;
+            Hunger = 5;
+            Happiness = 5;
+            Health = 10;
+        }
+    }
+}
